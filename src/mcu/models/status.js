5de68e67bbdfa40c274e228ad0bc4a0354f32b2d
Farhan P <farhan.abuhazan@gmail.com>
2019-01-22 19:18:54 +0700

    first commit

---
+++ src/mcu/models/status.js
@@ -0,0 +1,24 @@
+var datetime = {
+    date: '01/01/2018',
+    time: '00:00:00'
+}
+var freeMemory = 0;
+var gpio = [0,0,0,0,0,0];
+var btn = [0,0,0,0,0,0,0,0];
+var paracc = {}
+var waterStatus = {}
+var co2Status = {}
+var phStatus = {}
+var ecStatus = {}
+
+module.exports = {
+    freeMemory,
+    gpio,
+    btn,
+    paracc,
+    waterStatus,
+    co2Status,
+    phStatus,
+    ecStatus,
+    datetime
+}
